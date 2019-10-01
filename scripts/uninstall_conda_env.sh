ab6799d304df024113485df110a5811703b3769c
Jin Lee <leepc12@gmail.com>
2019-10-01 14:25:31 -0700

    conda env: atac -> cut-n-run

--- scripts/uninstall_conda_env.sh
+++ scripts/uninstall_conda_env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-CONDA_ENV_PY3=encode-atac-seq-pipeline
-CONDA_ENV_PY2=encode-atac-seq-pipeline-python2
-CONDA_ENV_OLD_PY3=encode-atac-seq-pipeline-python3
+CONDA_ENV_PY3=cut-n-run-pipeline
+CONDA_ENV_PY2=cut-n-run-pipeline-python2
+CONDA_ENV_OLD_PY3=cut-n-run-pipeline-python3
 
 conda env remove -n ${CONDA_ENV_PY3} -y
 conda env remove -n ${CONDA_ENV_PY2} -y
